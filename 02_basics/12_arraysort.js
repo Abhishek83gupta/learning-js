d46cd0c4422acfda16b4fb2883c108c98a7776bc
Abhishek Gupta <149913537+Abhishek83gupta@users.noreply.github.com>
2023-12-01 06:08:10 +0000

    array sort

---
+++ 02_basics/12_arraysort.js
@@ -0,0 +1,47 @@
+//................... Sorting an Array........................
+// The sort() method sorts an array alphabetically:
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits);
+
+
+// const number  = [13, 24, 35, 46, 54, 67, 77, 87, 94, 78];
+// number.sort();
+// console.log(number);
+
+
+const aplhabetnumber = [1, 2, 3, 4,"Abhishek", 67, "Gupta"];
+aplhabetnumber.sort();
+console.log(aplhabetnumber);            // Number priority fisrt 
+
+
+// const aplhabetnumber1 = [1, 2, 3, 4, "Abhishek",67, 0, 3.5];
+// aplhabetnumber1.sort();
+// console.log(aplhabetnumber1);
+
+
+//.................. Reversing an Array.......................
+// The reverse() method reverses the elements in an array.
+// You can use it to sort an array in descending order:
+
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+fruits1.sort();
+fruits1.reverse();
+console.log(fruits1);
+
+
+
+// const opposite = ["Mobile", "USB", "Rubixcube","Bluetooth"];
+// opposite.sort();                // sort
+// opposite.reverse();             // then reverse 
+// console.log(opposite);
+
+
+
+const opposite1 = ["Mobile", "USB", "Rubixcube","Bluetooth", 34, 92];
+opposite1.sort();               // 34 92 b m r u
+opposite1.reverse();            // reverse 
+console.log(opposite1);        
+
